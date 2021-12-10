c678b725da332b3a1ff965a424a4ff1c8be7faf9
Andrew Jackson <Andrew.Jackson@bl.uk>
2021-12-10 23:27:17 +0000

    Remove accidental MrJob vars.

--- entrypoint-h020.sh
+++ entrypoint-h020.sh
@@ -4,10 +4,6 @@
 export HADOOP_HOME=/usr/local/hadoop-0.20.2-cdh3u6
 export HADOOP_CONF_DIR=$HADOOP_HOME/etc/hadoop
 export PATH=$HADOOP_HOME/bin:$PATH
-export MRJOB_CONF=/etc/mrjob.conf
-
-#cat "192.168.1.103 h020nn" >> /etc/hosts
-#cat "192.168.1.104 h020jt" >> /etc/hosts
 
 exec "$@"
 
--- entrypoint-h3.sh
+++ entrypoint-h3.sh
@@ -3,7 +3,6 @@
 # Setup Hadoop 3 environment (conf pickup is automatic):
 export HADOOP_HOME=/usr/local/hadoop
 export PATH=$HADOOP_HOME/bin:$PATH
-export MRJOB_CONF=/etc/mrjob_h3.conf
 
 exec "$@"
 
